ac8a872d98c910e89496b2b7348129cc04cb45ee
Andrei Stratanovich <andreistratanovich@gmail.com>
2020-09-28 10:43:05 +0300

    Updates

---
+++ precache-manifest.24bfb45e00e1be12cfe583b004221ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ded0a6a8b856e7645d6636479fb818",
+    "url": "/1xslot-react/index.html"
+  },
+  {
+    "revision": "5ab8080f8832bc9a5204",
+    "url": "/1xslot-react/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "5ab8080f8832bc9a5204",
+    "url": "/1xslot-react/static/js/2.efc927f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/1xslot-react/static/js/2.efc927f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c9a987e87dee61a62b",
+    "url": "/1xslot-react/static/js/main.235eaf9d.chunk.js"
+  },
+  {
+    "revision": "573da5ad2d83ac132dc4",
+    "url": "/1xslot-react/static/js/runtime-main.d5306210.js"
+  }
+]);
